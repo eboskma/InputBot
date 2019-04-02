ea9b629dbeca01be9bf59e1aa4e99f0d5bd4537b
Rens Porre <rens.porre@gmail.com>
2019-04-02 19:51:32 +0200

    usize -> u64.

--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -218,7 +218,7 @@ fn handle_input_event(event: Event) {
     }
 }
 
-fn get_key_code(code: usize) -> u8 {
+fn get_key_code(code: u64) -> u8 {
     SEND_DISPLAY.with(|display| unsafe { XKeysymToKeycode(display, code) })
 }
 
